5cd40ac795da88d8d53f24bf9bc8fcfef7154ccc
AntonDatsik <Anton.Dacik@gmail.com>
2016-04-12 21:23:16 +0300

    implements project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetMe::Application.config.secret_key_base = '5615ff6b08f84189d827e9f09a1daae673a65469db3d1c80a0374304afae4104f110bcaa079449ad2681ac9a30dabedab42c370cd3c8929480a070c0d04fad49'
